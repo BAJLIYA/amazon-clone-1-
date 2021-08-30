520fdf9fba1a9eee37ed8ada6d8bfe1389e65ec3
BAJLIYA <banuraasel99@gmail.com>
2021-08-31 00:58:22 +0530

    8/31/2021

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    // BEM
+    <div className="App">
+      <h1>Heloo clever programmers,  lets build the amazon clone </h1>
+      {/*Header */}
+      {/*home */}    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDnbdKpHflsxBhfSYY-YDRpUqXq5_ThlhY",
+    authDomain: "challenge-aee70.firebaseapp.com",
+    projectId: "challenge-aee70",
+    storageBucket: "challenge-aee70.appspot.com",
+    messagingSenderId: "95912884780",
+    appId: "1:95912884780:web:518ced8a14a4b16fb0ef89",
+    measurementId: "G-GWKP924JN9"
+  };
